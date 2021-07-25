f752234f6b36dcce20739029159f583286431951
AliceS0103 <alice.underground87@gmail.com>
2021-07-25 14:17:24 +0300

    Syntax correction according to TF 1.0

--- test/test.tf
+++ test/test.tf
@@ -1,30 +1,37 @@
-resource "aws_vpc" "foo" {
+provider "aws" {
+  region = "eu-west-1"
+}
+/*resource "aws_vpc" "foo" {
   cidr_block = "10.1.0.0/16"
+}*/
+variable "foo" {
+  type = string
+  default = "vpc-014f24814d9f0e5a3"
 }
 
 data "aws_availability_zones" "available" {}
 
 resource "aws_subnet" "foo" {
-  vpc_id            = "${aws_vpc.foo.id}"
+  vpc_id            = var.foo
   cidr_block        = "10.1.1.0/24"
-  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+  availability_zone = data.aws_availability_zones.available.names[0]
 }
 
 resource "aws_subnet" "bar" {
-  vpc_id            = "${aws_vpc.foo.id}"
+  vpc_id            = var.foo
   cidr_block        = "10.1.2.0/24"
-  availability_zone = "${data.aws_availability_zones.available.names[1]}"
+  availability_zone = data.aws_availability_zones.available.names[1]
 }
 
 resource "aws_alb" "foo" {
   internal = true
-  subnets  = ["${aws_subnet.foo.id}", "${aws_subnet.bar.id}"]
+  subnets  = [aws_subnet.foo.id, aws_subnet.bar.id]
 }
 
 module "test-lb-waf" {
   source = "../"
 
-  load_balancer_arn = "${aws_alb.foo.arn}"
+  load_balancer_arn = aws_alb.foo.arn
 
   whitelist = [
     {
--- variables.tf
+++ variables.tf
@@ -1,24 +1,29 @@
 variable "module_enabled" {
+  type = string
   description = "Trigger to enable/disable module."
   default     = "true"
 }
 
 variable "project" {
+  type = string
   description = "Project name."
   default     = "project"
 }
 
 variable "environment" {
+  type = string
   description = "Environment name."
   default     = "test"
 }
 
 variable "resource_identifier" {
+  type = string
   description = "By default resource identifier is a sum of project name and environment name. This variable allows to override this with custom name."
   default     = "none"
 }
 
 variable "load_balancer_arn" {
+  type = string
   description = "ARN of Load Balancer to assign this WAF"
 }
 
